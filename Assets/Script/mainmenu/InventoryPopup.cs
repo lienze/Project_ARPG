4ea67879ec231fecefb5542575dbb185d2cd41f9
lienze <lienze2010@126.com>
2017-08-29 22:48:08 +0800

    其他物品信息的查看

---
+++ Assets/Script/mainmenu/InventoryPopup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryPopup : MonoBehaviour {
+
+    private UILabel nameLabel;
+    private UISprite inventorySprite;
+    private UILabel desLabel;
+    private UILabel btnLabel;
+    private InventoryItem it;
+
+    void Awake(){
+        nameLabel = transform.Find("Bg/NameLabel").GetComponent<UILabel>();
+        inventorySprite = transform.Find("Bg/Sprite/Sprite").GetComponent<UISprite>();
+        desLabel = transform.Find("Bg/Label").GetComponent<UILabel>();
+        btnLabel = transform.Find("Bg/ButtonUseBatching/Label").GetComponent<UILabel>();
+
+    }
+
+    public void Show(InventoryItem it){
+        this.gameObject.SetActive(true);
+        this.it = it;
+        nameLabel.text = it.Inventory.Name;
+        inventorySprite.spriteName = it.Inventory.ICON;
+        desLabel.text = it.Inventory.Des;
+        btnLabel.text = "批量使用(" + it.Count + ")";
+    }
+}
--- Assets/Script/mainmenu/Knapsack.cs
+++ Assets/Script/mainmenu/Knapsack.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class Knapsack : MonoBehaviour {
 
     private EquipPopup equipPopup;
+    private InventoryPopup inventoryPopup;
 
     void Awake(){
         equipPopup = transform.Find("EquipPopup").GetComponent<EquipPopup>();
+        inventoryPopup = transform.Find("InventoryPopup").GetComponent<InventoryPopup>();
     }
 
     public void OnInventoryClick(object[] objectArray){
         InventoryItem it = objectArray[0] as InventoryItem;
         bool isLeft = (bool)objectArray[1];
+        if (it.Inventory.InventoryTYPE == InventoryType.Equip) {
+            equipPopup.Show(it, isLeft);
+        } else {
+            inventoryPopup.Show(it);
+        }
 
-        equipPopup.Show(it, isLeft);
     }
 }
