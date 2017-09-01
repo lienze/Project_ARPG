70d5af596097b5295c536df869b3b180fc968f24
lienze <lienze2010@126.com>
2017-09-01 22:35:50 +0800

    任务的初始化

---
+++ Assets/Script/common/TaskManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskManager : MonoBehaviour {
+
+    public TextAsset taskinfoText;
+    private ArrayList taskList = new ArrayList();
+
+    /// <summary>
+    /// 初始化任务信息
+    /// </summary>
+    public void InitTask(){
+        string[] taskinfoArray = taskinfoText.ToString().Split('\n');
+        foreach (string str in taskinfoArray) {
+            string[] proArray = str.Split('|');
+            Task task = new Task();
+            task.Id = int.Parse(proArray[0]);
+            switch (proArray[1]) {
+                case "Main":
+                    task.TaskTYPE = TaskType.Main;
+                    break;
+                case "Reward":
+                    task.TaskTYPE = TaskType.Reward;
+                    break;
+                case "Daily":
+                    task.TaskTYPE = TaskType.Dally;
+                    break;
+            }
+            task.Name = proArray[2];
+            task.Icon = proArray[3];
+            task.Des = proArray[4];
+            task.Coin = int.Parse(proArray[5]);
+            task.Diamond = int.Parse(proArray[6]);
+            task.IdNpc = int.Parse(proArray[8]);
+            task.IdTranscript = int.Parse(proArray[9]);
+            taskList.Add(task);
+        }
+    }
+}
---
+++ Assets/Script/mainmenu/task/Task.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TaskType{
+    Main,//主线任务
+    Reward,//赏金任务
+    Dally//日常任务
+}
+
+public enum TaskProgress{
+    NoStart,
+    Accept,
+    Complete,
+    Reward
+}
+
+public class Task {
+
+    private int id;
+    private TaskType taskType;
+    private string name;
+    private string icon;
+    private string des;
+    private int coin;
+    private int diamond;
+    private string talkNpc;
+    private int idNpc;
+    private int idTranscript;
+    private TaskProgress taskProgress = TaskProgress.NoStart;
+
+    public int Id {
+        get {
+            return id;
+        }
+        set {
+            id = value;
+        }
+    }
+
+    public TaskType TaskTYPE {
+        get {
+            return taskType;
+        }
+        set {
+            taskType = value;
+        }
+    }
+
+    public string Name {
+        get {
+            return name;
+        }
+        set {
+            name = value;
+        }
+    }
+
+    public string Icon {
+        get {
+            return icon;
+        }
+        set {
+            icon = value;
+        }
+    }
+
+    public string Des {
+        get {
+            return des;
+        }
+        set {
+            des = value;
+        }
+    }
+
+    public int Coin {
+        get {
+            return coin;
+        }
+        set {
+            coin = value;
+        }
+    }
+
+    public int Diamond {
+        get {
+            return diamond;
+        }
+        set {
+            diamond = value;
+        }
+    }
+
+    public string TalkNpc {
+        get {
+            return talkNpc;
+        }
+        set {
+            talkNpc = value;
+        }
+    }
+
+    public int IdNpc {
+        get {
+            return idNpc;
+        }
+        set {
+            idNpc = value;
+        }
+    }
+
+    public int IdTranscript {
+        get {
+            return idTranscript;
+        }
+        set {
+            idTranscript = value;
+        }
+    }
+
+    public TaskProgress TaskPROGRESS {
+        get {
+            return taskProgress;
+        }
+        set {
+            taskProgress = value;
+        }
+    }
+}
