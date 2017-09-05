99d1b547201d0e2bfdfbc554272b6d73d1d471c9
lienze <lienze2010@126.com>
2017-09-05 22:20:19 +0800

    增加男性角色自动寻路

---
+++ Assets/PlayerAutoMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerAutoMove : MonoBehaviour {
+
+    private NavMeshAgent agent;
+    public float minDistance = 3;
+
+    public Transform target;
+
+    void Start(){
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    void Update(){
+        if (agent.enabled) {
+            if (agent.remainingDistance < minDistance) {
+                agent.isStopped = true;
+                agent.enabled = false;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+            SetDestination(target.position);
+        }
+    }
+
+    public void SetDestination(Vector3 targetPos){
+        agent.enabled = true;
+        agent.SetDestination(targetPos);
+    }
+}
