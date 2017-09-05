c924853b4ae5eb24e29ae8720f94a74feba51388
lienze <lienze2010@126.com>
2017-09-05 09:22:01 +0800

    控制角色行走

---
+++ Assets/Script/player/PlayerMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float velcoity = 5;
+    Rigidbody rigidBody;
+    void Awake(){
+        rigidBody = this.GetComponent<Rigidbody>();
+    }
+
+    void Update(){
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 vel = rigidBody.velocity;
+        rigidBody.velocity = new Vector3(-h*velcoity, vel.y, v*velcoity);
+    }
+
+}
