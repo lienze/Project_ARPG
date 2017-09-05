c7d436ac63f266d7fd51aaae2f25c48b385b36fb
lienze <lienze2010@126.com>
2017-09-05 20:52:17 +0800

    控制角色动画的播放

---
+++ Assets/Script/player/PlayerVillageAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVillageAnimation : MonoBehaviour {
+
+    private Animator anim;
+    private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        anim = this.GetComponent<Animator>();
+        rigidBody = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (rigidBody.velocity.magnitude > 0.5f) {
+            anim.SetBool("Move", true);
+        } else {
+            anim.SetBool("Move", false);
+        }
+	}
+}
--- Assets/Script/player/PlayerVillageMove.cs
+++ Assets/Script/player/PlayerVillageMove.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour {
+public class PlayerVillageMove : MonoBehaviour {
 
     public float velcoity = 5;
     Rigidbody rigidBody;
@@ -14,7 +14,11 @@ public class PlayerMove : MonoBehaviour {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         Vector3 vel = rigidBody.velocity;
-        rigidBody.velocity = new Vector3(-h*velcoity, vel.y, v*velcoity);
+        rigidBody.velocity = new Vector3(-h*velcoity, vel.y, -v*velcoity);
+
+        if (Mathf.Abs(h) > 0.05f || Mathf.Abs(v) > 0.05f) {
+            transform.rotation = Quaternion.LookRotation(new Vector3(-h, 0, -v));
+        }
     }
 
 }
